d85e79b98cbb4d83647a179f1edb8cbad5608cd3
javasegquasex <javasegquasex@GYNLAB-JED08>
2017-10-19 20:59:43 -0200

    inicio do projeto com git ignore.

---
+++ projeto_calculadora/src/br/com/threeway/Calculadora.java
@@ -0,0 +1,27 @@
+package br.com.threeway;
+
+public class Calculadora {
+	private String nome = "Calc dos alunos ";
+	
+	public static final String VERSAO = "1.0";
+	
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+	    this.nome = nome;	
+	}
+    public int dividir(String num1, int num2) {
+    	return 0;
+    }
+    public int somar(int num1, int num2) {
+    	return 0;
+    }
+    public int subtrair (int i , int j) {
+    	return 0;
+    }
+    public int multiplicar(int i , int j) {
+    	return 0;
+    }
+}
---
+++ projeto_calculadora/src/br/com/threeway/Main.java
@@ -0,0 +1,15 @@
+package br.com.threeway;
+
+public class Main {
+   
+	    public static void main (String [] args) {
+	//	System.out.println("testando a calculadora .. ");
+		
+	    Calculadora calc = new Calculadora ();
+	    calc.getNome();
+	    int resultado = calc.somar(6, 2);
+	    resultado = calc.dividir
+        
+	  }
+		
+}
